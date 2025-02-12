dcf8c946e05a56d5a5728176024899306a0a2d91
Danilll1 <gabrick_72@mail.ru>
2025-02-12 11:24:12 +0500

    folders

---
+++ app/Http/Controllers/FileController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\File;
+use App\Models\User;
+use App\Models\Folder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class FileController extends Controller
+{
+    public function index()
+    {
+        $folders = Folder::where('user_id', Auth::user()->id)->get();
+        $files = File::where('user_id', Auth::user()->id)->get();
+        return view('files', compact('files', 'folders'));
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|file'
+        ]);
+    
+        $folderName = Auth::user()->name . '-' . Auth::user()->id . '-' . 'folder';
+    
+        if (!\Storage::disk('public')->exists('uploads/' . $folderName)) {
+            \Storage::disk('public')->makeDirectory('uploads/' . $folderName);
+        }
+    
+        $file = $request->file('file');
+        $fileName = $file->getClientOriginalName();
+        $path = $file->storeAs('uploads/' . $folderName, $fileName, 'public');
+        
+        $userModel = Auth::user();
+        $userModel->storage_name = $folderName;
+        $userModel->save();
+
+        $fileModel = new File;
+        $fileModel->name = $fileName;
+        $fileModel->path = $path;
+        $fileModel->user_id = Auth::user()->id;
+        $fileModel->save();
+    
+        return redirect()->back()->with('success', 'Файл успешно загружен!');
+    }
+    
+    public function folderStore(Request $request) {
+        $validatedData = $request->validate([
+            'foldersName' => 'required|string|max:255'
+        ]);
+    
+        $foldersName = $validatedData['foldersName'];
+        $folderName = Auth::user()->name . '-' . Auth::user()->id . '-' . 'folder';
+
+        if (!\Storage::disk('public')->exists('uploads/' . $folderName)) {
+            \Storage::disk('public')->makeDirectory('uploads/' . $folderName);
+        }
+
+        $userModel = Auth::user();
+        $userModel->storage_name = $folderName;
+        $userModel->save();
+        
+        \Storage::disk('public')->makeDirectory('uploads/' . Auth::user()->storage_name . '/' . $foldersName);
+        $folder = new Folder;
+        $folder->name = $foldersName;
+        $folder->user_id = Auth::user()->id;
+        $folder->path = 'uploads/'. $folderName . '/' . $foldersName;
+        $folder->save();
+        return redirect()->back()->with('success', 'Папка успешно создана!');
+    }
+
+    public function folderAdd(Request $request)
+    {
+        $folderId = $request->folderId;
+        $folder = Folder::find($folderId);
+        $file = $request->file('file');
+        $fileName = $file->getClientOriginalName();
+        $folderName = Auth::user()->storage_name;
+        $path = $file->storeAs($folderName . '/' . $folder->path, $fileName, 'public');
+
+        $fileModel = new File;
+        $fileModel->name = $fileName;
+        $fileModel->path = $path;
+        $fileModel->user_id = Auth::user()->id;
+        $fileModel->folder_id = $folderId;
+        $fileModel->save();
+        return redirect()->back()->with('success', 'Файл успешно добавлен в папку!');
+    }
+
+    public function show(string $id)
+    {
+        //
+    }
+
+    public function edit(string $id)
+    {
+        //
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        $file = File::find($id);
+        if ($file->user_id == Auth::user()->id) {
+            if (\Storage::disk('public')->exists($file->path)) {
+                \Storage::disk('public')->delete($file->path);
+            }
+            $file->delete();
+            return redirect()->back()->with('success', 'Файл успешно удален!');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,24 @@
 <?php
 
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\ListController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/home', [ListController::class, 'index'])->name('home');
 Route::get('/statuses', [ListController::class, 'statuses'])->name('statuses');
-
-
 Auth::routes();
 
 Route::post('/edit/{id}', [ListController::class, 'edit'])->name('edit');
 Route::post('/create', [ListController::class, 'create'])->name('create');
-
 Route::delete('/tasks/{id}', [ListController::class, 'destroy'])->name('delete');
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/files', [FileController::class,'index'])->name('files');
+Route::post('/files-store', [FileController::class, 'store'])->name('store');
+
+Route::post('/folders-store', [FileController::class, 'folderStore'])->name('folder.store');
+Route::post('/folders-add', [FileController::class, 'folderAdd'])->name('folder.add');
+
+Route::delete('/files/{id}', [FileController::class,'destroy'])->name('destroy.file');
